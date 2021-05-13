b91f46e61709c18c351c13bc49812e0d02281165
Yannick Pflanzer <ypflanzer@outlook.com>
2021-05-13 13:01:02 +0200

    Updated where tests are executed and ported CPU tests to SYCL

--- cvsycl/test/DeviceTest.cpp
+++ cvsycl/test/DeviceTest.cpp
@@ -9,11 +9,14 @@
 #include <cvpp/CommonFilters.h>
 
 #include <cvpp/cvsycl/StructureTensor.h>
+#include <cvpp/cvsycl/HarrisDetector.h>
+
+#define TESTIMG "../test1.png"
 
 class kernel_name;
 
-// https://techdecoded.intel.io/resources/programming-data-parallel-c/#gs.mflkxv
 #if 0
+// https://techdecoded.intel.io/resources/programming-data-parallel-c/#gs.mflkxv
 TEST(SYCL, Image)
 {
 	//cl::sycl::queue q(cl::sycl::gpu_selector{});
@@ -28,8 +31,21 @@ TEST(SYCL, Image)
 
 	img2.save("testsycl.png");
 }
+
+TEST(SYCL, Sobel)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img("test1.png");
+	auto gr = cvsycl::ConvertType<uint8_t, float>(img, q);
+
+	gr = cvsycl::MakeGrayscale(gr, q);
+	gr = cvsycl::Convolute2D<cvsycl::ClampView<float>>(gr, cvpp::SobelFilterH(), q);
+	
+	cvsycl::ConvertType<float, uint8_t>(gr, q).save("test1_sobel_x_sycl.png");
+}
 #endif
 
+#if 0
 TEST(SYCL, StructureTensor)
 {
 	//cl::sycl::queue q(cl::sycl::gpu_selector{});
@@ -46,4 +62,176 @@ TEST(SYCL, StructureTensor)
 	auto saveImg = cvsycl::ConvertType<float, uint8_t>(out, q);
 	saveImg.save("sycl_determinant.png");
 }
+#endif
 
+TEST(Detector, Harris)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+
+	cvpp::Image<uint8_t> img(TESTIMG);
+	cvsycl::Image<uint8_t> gpuImg(img);
+
+	std::vector<cvpp::Feature> features;
+	cvsycl::HarrisDetector(gpuImg, 11, 1.0f, features, q);
+
+	cvpp::MarkFeatures(img, Eigen::Vector4f(1, 0, 1, 1), features);
+	img.save("DetectorHarris.png");
+	std::cout << "FEATURES: " << features.size() << std::endl;
+}
+
+TEST(Image, LoadSave)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	img.save("ImageLoadSave.png");
+	img.save("ImageLoadSave.jpg");
+	img.save("ImageLoadSave.tga");
+	
+	EXPECT_THROW(img.save("ImageLoadSave.hdr"), std::runtime_error);
+}
+
+TEST(Image, ConvertType)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	auto shortImg = ConvertType<uint8_t, float>(img, q);
+	shortImg.save("ImageConvertType.hdr");
+}
+
+TEST(Convolution, Conv1D)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+
+	cvsycl::Image<uint8_t> img(TESTIMG);
+	auto grey = MakeGrayscale(img, q);
+
+	Eigen::Vector3f krnl;
+	krnl << -0.5, 0, 0.5;
+
+	auto out = Convolute1D<cvsycl::ClampView<uint8_t>, cvsycl::HORIZONTAL>(img, krnl, q);
+	auto out2 = Convolute1D<cvsycl::ClampView<uint8_t>, cvsycl::VERTICAL>(img, krnl, q);
+
+	out.save("ConvolutionConv1DHorizontal.png");
+	out2.save("ConvolutionConv1DVertical.png");
+}
+
+TEST(Convolution, Conv2D)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	Eigen::Matrix3f mtx;
+	mtx.fill(1.0f/9.0f);
+
+	auto out = Convolute2D<cvsycl::ClampView<uint8_t>>(img, mtx, q);
+	out.save("ConvolutionConv2D.png");
+}
+
+TEST(Convolution, Conv2DMat3)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	Eigen::Matrix3f mtx;
+	mtx.fill(1.0f/9.0f);
+
+	auto out = Convolute2D<cvsycl::ClampView<uint8_t>>(img, mtx, q);
+
+	out.save("ConvolutionConv2DMat3.png");
+}
+
+TEST(Convolution, ConvSep)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+
+	cvsycl::Image<uint8_t> img(TESTIMG);
+	auto out = ConvoluteSeparable<cvsycl::ClampView<uint8_t>>(img, cvpp::BoxFilter<5>(), q);
+
+	out.save("ConvolutionConvSep.png");
+}
+
+TEST(Convolution, GaussFilter)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+	auto out = ConvoluteSeparable<cvsycl::ClampView<uint8_t>>(img, cvpp::GaussFilter<11>(2.0f), q);
+
+	out.save("ConvolutionGaussFilter.png");
+}
+
+TEST(Convolution, StackBlur)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	constexpr int sz = 15;
+	auto out = ConvoluteSeparable<cvsycl::ClampView<uint8_t>>(img, cvpp::StackBlurFilter<sz>(), q);
+
+	out.save("ConvolutionStackBlur.png");
+}
+
+TEST(Convolution, BlurAndEdge)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	img = MakeGrayscale(img, q);
+	img = ConvoluteSeparable<cvsycl::ClampView<uint8_t>>(img, cvpp::StackBlurFilter<9>(), q);
+	img = Convolute1D<cvsycl::ClampView<uint8_t>, cvsycl::HORIZONTAL>(img, cvpp::SimpleEdgeDetector, q);
+
+	img.save("ConvolutionBlurAndEdge.png");
+}
+
+TEST(Convert, Grayscale)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+	auto gr = MakeGrayscale(img, q);
+
+	gr.save("ConvertGrayscale.png");
+}
+
+TEST(Convolution, Sobel)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+	auto gr = MakeGrayscale(img, q);
+	gr = Convolute2D<cvsycl::ClampView<uint8_t>>(gr, cvpp::SobelFilterH(), q);
+	gr.save("ConvolutionSobelH.png");
+
+	gr = Convolute2D<cvsycl::ClampView<uint8_t>>(gr, cvpp::SobelFilterV(), q);
+	gr.save("ConvolutionSobelV.png");
+}
+
+#if 0
+TEST(Detector, Hessian)
+{
+	cvsycl::Image<uint8_t> img(TESTIMG);
+
+	std::vector<cvpp::Feature> features;
+	cvpp::HessianDetector(img, 21, 0.5f, features);
+
+	cvpp::MarkFeatures(img, Eigen::Vector4f(1, 0, 1, 1), features);
+	img.save("DetectorHessian.png");
+	std::cout << "FEATURES: " << features.size() << std::endl;
+}
+#endif
+
+TEST(Convolution, Laplace)
+{
+	cl::sycl::queue q(cl::sycl::default_selector{});
+	cvsycl::Image<uint8_t> img(TESTIMG);
+	auto gr = MakeGrayscale(img, q);
+
+	auto Dx = Convolute1D<cvsycl::ClampView<uint8_t>, cvsycl::HORIZONTAL>(gr, cvpp::LaplaceFilterX, q);
+	auto Dy = Convolute1D<cvsycl::ClampView<uint8_t>, cvsycl::VERTICAL>(gr, cvpp::LaplaceFilterX, q);
+	auto Dxy = Convolute2D<cvsycl::ClampView<uint8_t>>(gr, cvpp::LaplaceFilterXY(), q);
+
+	Dx.save("ConvolutionLaplaceX.png");
+	Dy.save("ConvolutionLaplaceY.png");
+	Dxy.save("ConvolutionLaplaceXY.png");
+}
--- test/DeviceTest.cpp
+++ test/DeviceTest.cpp
@@ -8,28 +8,30 @@
 
 #include <Eigen/Dense>
 
-TEST(Image, Load)
-{
-	cvpp::Image<uint8_t> img("test1.png");
+#define TESTIMG "../test1.png"
 
-	img.save("testsave.png");
-	img.save("testsave.jpg");
-	img.save("testsave.tga");
+TEST(Image, LoadSave)
+{
+	cvpp::Image<uint8_t> img(TESTIMG);
+
+	img.save("ImageLoadSave.png");
+	img.save("ImageLoadSave.jpg");
+	img.save("ImageLoadSave.tga");
 	
-	EXPECT_THROW(img.save("testsave.hdr"), std::runtime_error);
+	EXPECT_THROW(img.save("ImageLoadSave.hdr"), std::runtime_error);
 }
 
 TEST(Image, ConvertType)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 
 	auto shortImg = ConvertType<uint8_t, float>(img);
-	shortImg.save("testsave.hdr");
+	shortImg.save("ImageConvertType.hdr");
 }
 
 TEST(Sampler, ClampSampler)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	auto sampler = cvpp::ClampView(img);
 
 	auto c1 = sampler.sample(0.0, 0.0);
@@ -50,7 +52,7 @@ TEST(Utils, Mod)
 
 TEST(Convolution, Conv1D)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	auto grey = cvpp::MakeGrayscale(img);
 	cvpp::ClampView sampler(grey);
 
@@ -60,13 +62,13 @@ TEST(Convolution, Conv1D)
 	auto out = cvpp::Convolute1D(sampler, krnl);
 	auto out2 = cvpp::Convolute1D<cvpp::VERTICAL>(sampler, krnl);
 
-	out.save("test1_convolved_1d_hor.png");
-	out2.save("test1_convolved_1d_vert.png");
+	out.save("ConvolutionConv1DHorizontal.png");
+	out2.save("ConvolutionConv1DVertical.png");
 }
 
 TEST(Convolution, Conv2D)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	cvpp::ClampView sampler(img);
 
 	Eigen::MatrixXf mtx(7, 7);
@@ -74,12 +76,12 @@ TEST(Convolution, Conv2D)
 
 	auto out = cvpp::Convolute2D(sampler, mtx);
 
-	out.save("test1_conv2d.png");
+	out.save("ConvolutionConv2D.png");
 }
 
 TEST(Convolution, Conv2DMat3)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	cvpp::ClampView sampler(img);
 
 	Eigen::Matrix3f mtx;
@@ -87,74 +89,74 @@ TEST(Convolution, Conv2DMat3)
 
 	auto out = cvpp::Convolute2D(sampler, mtx);
 
-	out.save("test1_conv2d_mat3.png");
+	out.save("ConvolutionConv2DMat3.png");
 }
 
 TEST(Convolution, ConvSep)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	cvpp::ClampView sampler(img);
 
 	auto out = cvpp::ConvoluteSeparable(sampler, cvpp::BoxFilter<5>());
 
-	out.save("test1_convsep.png");
+	out.save("ConvolutionConvSep.png");
 }
 
 TEST(Convolution, GaussFilter)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	cvpp::ClampView sampler(img);
 
 	auto out = cvpp::ConvoluteSeparable(sampler, cvpp::GaussFilter<11>(2.0f));
 
-	out.save("test1_gauss.png");
+	out.save("ConvolutionGaussFilter.png");
 }
 
 TEST(Convolution, StackBlur)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	cvpp::ClampView sampler(img);
 
 	constexpr int sz = 15;
 	auto out = cvpp::ConvoluteSeparable(sampler, cvpp::StackBlurFilter<sz>());
 
-	out.save("test1_stackblur.png");
+	out.save("ConvolutionStackBlur.png");
 }
 
 TEST(Convolution, BlurAndEdge)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	cvpp::ClampView sampler(img);
 
 	img = cvpp::MakeGrayscale(img);
 	img = cvpp::ConvoluteSeparable(sampler, cvpp::StackBlurFilter<9>());
 	img = cvpp::Convolute1D(sampler, cvpp::SimpleEdgeDetector, 3);
 
-	img.save("test1_edges_hor.png");
+	img.save("ConvolutionBlurAndEdge.png");
 }
 
 TEST(Convert, Grayscale)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	auto gr = cvpp::MakeGrayscale(img);
 
-	gr.save("test1_greyscale.png");
+	gr.save("ConvertGrayscale.png");
 }
 
 TEST(Convolution, Sobel)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	auto gr = cvpp::MakeGrayscale(img);
 	gr = cvpp::Convolute2D(cvpp::ClampView(gr), cvpp::SobelFilterH());
-	gr.save("test1_sobel_x.png");
+	gr.save("ConvolutionSobelH.png");
 
 	gr = cvpp::Convolute2D(cvpp::ClampView(gr), cvpp::SobelFilterV());
-	gr.save("test1_sobel_y.png");
+	gr.save("ConvolutionSobelV.png");
 }
 
 TEST(Structure, StructureTensor)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	auto tensor = cvpp::StructureTensor(img);
 
 	float max = 0.0f;
@@ -178,36 +180,36 @@ TEST(Structure, StructureTensor)
 	});
 
 	std::cout << "Max: " << max << std::endl;
-	(img * cvpp::MakeRGB(cvpp::ConvertType<float, unsigned char>(determinant))).save("structure_determinant.png");
+	(img * cvpp::MakeRGB(cvpp::ConvertType<float, unsigned char>(determinant))).save("StructureStructureTensor.png");
 }
 
 TEST(Detector, Harris)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 
 	std::vector<cvpp::Feature> features;
 	cvpp::HarrisDetector(img, 11, 1.1f, features);
 
 	cvpp::MarkFeatures(img, Eigen::Vector4f(1, 0, 1, 1), features);
-	img.save("HarrisFeatures.png");
+	img.save("DetectorHarris.png");
 	std::cout << "FEATURES: " << features.size() << std::endl;
 }
 
 TEST(Detector, Hessian)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 
 	std::vector<cvpp::Feature> features;
 	cvpp::HessianDetector(img, 21, 0.5f, features);
 
 	cvpp::MarkFeatures(img, Eigen::Vector4f(1, 0, 1, 1), features);
-	img.save("HessianFeatures.png");
+	img.save("DetectorHessian.png");
 	std::cout << "FEATURES: " << features.size() << std::endl;
 }
 
 TEST(Convolution, Laplace)
 {
-	cvpp::Image<uint8_t> img("test1.png");
+	cvpp::Image<uint8_t> img(TESTIMG);
 	auto gr = cvpp::MakeGrayscale(img);
 
 	cvpp::ClampView sampler(gr);
@@ -216,7 +218,7 @@ TEST(Convolution, Laplace)
 	auto Dy = Convolute1D<cvpp::VERTICAL>(sampler, cvpp::LaplaceFilterX);
 	auto Dxy = Convolute2D(sampler, cvpp::LaplaceFilterXY());
 
-	Dx.save("test1_laplace_x.png");
-	Dy.save("test1_laplace_y.png");
-	Dxy.save("test1_laplace_xy.png");
+	Dx.save("ConvolutionLaplaceX.png");
+	Dy.save("ConvolutionLaplaceY.png");
+	Dxy.save("ConvolutionLaplaceXY.png");
 }
